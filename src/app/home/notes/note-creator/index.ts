fd9493ff64bbc4ef3446fe27b7bc38b695e67444
Yishan Wen <wittywys@gmaill.com>
2016-09-08 17:34:12 +0800

    basic note creater

---
+++ src/app/home/notes/note-creator/index.ts
@@ -0,0 +1,15 @@
+import  { Component } from '@angular/core'
+
+@Component({
+  selector: 'note-creator',
+  template: require('./note-creator.html'),
+  styles: [ require('./note-creator.css') ]
+})
+export class NoteCreator {
+  newNote = { title: '', value: ''};
+
+  createNote() {
+    console.log(this.newNote);
+    return false;
+  }
+}
