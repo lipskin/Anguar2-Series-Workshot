1220e58c367a65e8727340ab4204b87bcf1ac490
Xing <yellowstar1992@hotmail.com>
2016-09-22 18:35:40 +0800

    create and get service

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,10 @@ import {bootstrap} from '@angular/platform-browser-dynamic'
 import { disableDeprecatedForms, provideForms } from '@angular/forms'
 
 import {App, providers} from './app'
+import {HTTP_PROVIDERS} from "@angular/http";
 
 bootstrap(App, [
+  ...HTTP_PROVIDERS,
   disableDeprecatedForms(),
   provideForms(),
   ...providers
