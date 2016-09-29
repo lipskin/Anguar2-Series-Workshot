8726c420ccc2b9efd4845dd61c0ee8c263e7c759
Trotyl <trotyl@qq.com>
2016-09-29 09:42:25 +0800

    Create auth component.

---
+++ lesson-4.md
@@ -0,0 +1,7 @@
+#Steps
+
+##Part 1
+
+- Create folder `auth` in `src/app`;
+- Create file `auth.component.ts`, `auth.component.html` and `index.ts` in `src/app/auth`;
+- Add basic content to `auth.component.ts`, `auth.component.html` and `index.ts`;
---
+++ src/app/auth/auth.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core'
+
+@Component({
+    selector: 'auth',
+    template: require('./auth.component.html')
+})
+export class AuthComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit() { }
+}
