ca7dba6a393daafb34ced9990086bad313001392
Trotyl <trotyl@qq.com>
2016-09-29 10:19:48 +0800

    Add route for auth component.

--- lesson-4.md
+++ lesson-4.md
@@ -5,3 +5,5 @@
 - Create folder `auth` in `src/app`;
 - Create file `auth.component.ts`, `auth.component.html` and `index.ts` in `src/app/auth`;
 - Add basic content to `auth.component.ts`, `auth.component.html` and `index.ts`;
+- Add a route in `src/app/routes.ts`;
+
--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,9 +1,11 @@
 import {RouterConfig} from '@angular/router'
 import {Home} from './home'
 import {About} from './about'
+import { AuthComponent } from './auth'
 
 export const router: RouterConfig = [
   {path: '', component: Home},
   {path: 'about', component: About},
+  {path: 'auth', component: AuthComponent},
   {path: '**', redirectTo: ''}
 ]
